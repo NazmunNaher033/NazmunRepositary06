53a948791f76cd53d0baf6c9944b9b3e2aeb2d50
NazmunNaher1999 <nazmunna033@gmail.com>
2023-10-01 00:44:35 -0400

    updated

---
+++ src/test/java/base/gitPractice02.java
@@ -0,0 +1,8 @@
+package base;
+
+public class gitPractice02 {
+    public static void main(String[] args) {
+        System.out.println("Nazmun Git project");
+        System.out.println("my country is best");
+    }
+}
